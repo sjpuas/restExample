c3a9749e88d5a1ec90ba4cc46a3421cb02ecd0dd
Sergio Puas <sergio@puas.cl>
2017-01-12 14:02:31 -0300

    add binaries

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -t -v $GOPATH/src:/go/src -v $(pwd):/go/bin golang:1.7.3 /bin/bash -c 'CGO_ENABLED=0 GOOS=linux go install -a -installsuffix cgo restExample'
+
+docker run -t -v $GOPATH/src:/go/src -v $(pwd):/go/bin golang:1.7.3 /bin/bash -c 'CGO_ENABLED=0 GOOS=linux go install -a -installsuffix cgo github.com/sjpuas/restExample'
