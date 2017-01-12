0517845b9fe7967840c982e9823f3393a8c7a402
Sergio Puas <sergio@puas.cl>
2017-01-12 13:35:55 -0300

    intial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM scratch
+ADD restExample /
+EXPOSE 8080
+CMD ["/restExample"]
---
+++ make.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -t -v $GOPATH/src:/go/src -v $(pwd):/go/bin golang:1.7.3 /bin/bash -c 'CGO_ENABLED=0 GOOS=linux go install -a -installsuffix cgo restExample'
